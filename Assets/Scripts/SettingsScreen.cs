95e295fcebca5182731f5d27d8c70b2c0c0a27bc
Dmitriy Prikhodko <bojlahg@gmail.com>
2017-05-29 13:30:25 +0500

    initial

---
+++ Assets/Scripts/SettingsScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScreen : MonoBehaviour
+{
+	public void Button_Start()
+	{
+		
+	}
+	
+	public void Button_IncreaseMin()
+	{
+		
+	}
+
+	public void Button_DecreaseMin()
+	{
+
+	}
+
+	public void Button_IncreaseMax()
+	{
+
+	}
+
+	public void Button_DecreaseMax()
+	{
+
+	}
+
+	public void Toggle_Order(bool val)
+	{
+
+	}
+}
